b720061c75916a2797951d07d672aa28d84f7f46
Ahmet Alpaslan Genç <ahmetalpaslangenc@Ahmet-MacBook-Pro-5.local>
2024-06-06 10:15:14 +0300

    First commit

---
+++ Models/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CetBooks.Models
+{
+    public class Book
+    {
+        [DisplayName("Kitap No")]
+        public int Id { get; set; }
+
+        [DisplayName("Kitap Adı")]
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [DisplayName("Yazar")]
+        [Required]
+        public string Author { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Publish Date")]
+        [Required]
+        public DateTime PublishDate { get; set; }
+
+        [DisplayName("Page Size")]
+        [Required]
+        public int PageSize { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
+
---
+++ Models/FakeDB.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CetBooks.Models
+{
+    public class FakeDB
+    {
+        public static List<Book> Db { get; set; } = new List<Book>();
+
+        static FakeDB() 
+        {
+            Db.Add(
+                new Book 
+                {
+                    Id = 1,
+                    Title = "C#",
+                    Description = "Programming Languages",
+                    Author = "Ahmet Yıldırım",
+                    PageSize = 200,
+                    Price = 100,
+                    PublishDate = DateTime.Now
+                }
+            );
+            Db.Add(
+                new Book
+                {
+                    Id = 2,
+                    Title = "Asp.net",
+                    Description = "Programming Languages",
+                    Author = "Ahmet Yıldırım",
+                    PageSize = 300,
+                    Price = 150,
+                    PublishDate = DateTime.Now
+                }
+            );
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return Db;
+        }
+
+        public Book? GetBookById(int id)
+        {
+            return Db.FirstOrDefault(b => b.Id == id);
+        }
+
+        public bool DeleteBook(int id)
+        {
+            var book = GetBookById(id);
+            if (book == null) return false;
+            Db.Remove(book);
+            return true;
+        }
+
+        public void AddBook(Book book)
+        {
+            book.Id = Db.Any() ? Db.Max(b => b.Id) + 1 : 1;
+            Db.Add(book);
+        }
+    }
+}
+
+
